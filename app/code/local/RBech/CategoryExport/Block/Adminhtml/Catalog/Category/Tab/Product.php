095228e36b5581cab96ef1dacd1de784bda0a94b
Rasmus Bech <me@rbech.com>
2016-11-30 13:49:20 +0100

    Switch to a block rewrite instead to allow us to remove columns from export

---
+++ app/code/local/RBech/CategoryExport/Block/Adminhtml/Catalog/Category/Tab/Product.php
@@ -0,0 +1,58 @@
+<?php
+class RBech_CategoryExport_Block_Adminhtml_Catalog_Category_Tab_Product extends Mage_Adminhtml_Block_Catalog_Category_Tab_Product
+{
+    /**
+     * Override _prepareColumns and set is_system to true
+     * for is_category and position, this prevents them from
+     * being exported
+     *
+     * @return void
+     */
+    protected function _prepareColumns()
+    {
+        if (!$this->getCategory()->getProductsReadonly()) {
+            $this->addColumn('in_category', array(
+                'header_css_class' => 'a-center',
+                'type'      => 'checkbox',
+                'name'      => 'in_category',
+                'values'    => $this->_getSelectedProducts(),
+                'align'     => 'center',
+                'index'     => 'entity_id',
+                'is_system' => true,
+            ));
+        }
+        $this->addColumn('entity_id', array(
+            'header'    => Mage::helper('catalog')->__('ID'),
+            'sortable'  => true,
+            'width'     => '60',
+            'index'     => 'entity_id'
+        ));
+        $this->addColumn('name', array(
+            'header'    => Mage::helper('catalog')->__('Name'),
+            'index'     => 'name'
+        ));
+        $this->addColumn('sku', array(
+            'header'    => Mage::helper('catalog')->__('SKU'),
+            'width'     => '80',
+            'index'     => 'sku'
+        ));
+        $this->addColumn('price', array(
+            'header'    => Mage::helper('catalog')->__('Price'),
+            'type'  => 'currency',
+            'width'     => '1',
+            'currency_code' => (string) Mage::getStoreConfig(Mage_Directory_Model_Currency::XML_PATH_CURRENCY_BASE),
+            'index'     => 'price'
+        ));
+        $this->addColumn('position', array(
+            'header'    => Mage::helper('catalog')->__('Position'),
+            'width'     => '1',
+            'type'      => 'number',
+            'index'     => 'position',
+            'editable'  => !$this->getCategory()->getProductsReadonly(),
+            //'renderer'  => 'adminhtml/widget_grid_column_renderer_input'
+        ));
+
+        //Add export type for CSV
+        $this->addExportType('*/*/exportCsv', Mage::helper('adminhtml')->__('CSV'));
+    }
+}
